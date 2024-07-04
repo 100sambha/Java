5bf4d3e53d03c068062ff24f52301d5fd6e186b5
Sambhaji Shivaji Pandhare <sambhaji.pandhare@techwave.net>
2024-07-05 05:07:03 +0530

    Hello World

---
+++ core/src/main/java/com/core/App.java
@@ -0,0 +1,16 @@
+package com.core;
+
+/**
+ * Author
+ * Sambhaji Pandhare
+ * Software Engineer
+ * +91 841 104 1938
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
